8d33789b7048adc921bc15ad2bc96d71782b4710
Vignesh dandu <VIGNESHDANDU8355@GMAIL.COM>
2025-12-04 10:22:54 +0000

    Add multiple JavaScript function examples including addition, multiplication, greeting, and object constructors

---
+++ arrow_function/arrowfunction.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a + b;
+console.log(add(5, 19))
+
+const multiply =(a,b)=> a*b;
+console.log(multiply(5,6));
+
+const greet = () => "hello world";
+console.log(greet());
+ 
+let x = 10 
+const y = 10
+console.log(`${x + y}`);
---
+++ functions/toKelvin.js
@@ -0,0 +1,6 @@
+function toKelvin(celsius) {
+    return celsius + 273.15;
+}
+console.log(toKelvin(25)); // Example usage
+
+
